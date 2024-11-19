3e629514c23a8334becc0afb02e060e8540f8fbd
Mark Turner <turner.mark@abc.net.au>
2024-11-19 16:41:52 +0930

    Provide missing variables.

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	"github.com/bitrise-io/go-steputils/stepconf"
-	"github.com/bitrise-io/go-utils/log"
 )
 
 type config struct {
@@ -19,12 +18,29 @@ type config struct {
 }
 
 func triggerPipeline(cfg config) error {
-	apiURL := fmt.Sprintf("%s/projects/%s/trigger/pipeline", cfg.APIBaseURL, url.PathEscape(cfg.ProjectID))
+	apiURL := fmt.Sprintf("%s/projects/%s/trigger/pipeline", cfg.APIBaseURL, cfg.ProjectID)
 
-	data := url.Values{
-		"token": {cfg.PrivateToken},
-		"ref":   {cfg.GitRef},
+	// Determine the ref to use
+	ref := cfg.GitRef
+	if ref == "" {
+		ref = os.Getenv("BITRISE_GIT_BRANCH")
+		fmt.Printf("DEBUG: Using BITRISE_GIT_BRANCH as ref: %s\n", ref)
 	}
+	if ref == "" {
+		return fmt.Errorf("ref is required but not provided and BITRISE_GIT_BRANCH is also empty")
+	}
+
+	// Prepare form data with variables
+	data := url.Values{
+		"token":                         {cfg.PrivateToken},
+		"ref":                           {ref},
+		"variables[BITRISE_API_TOKEN]":  {os.Getenv("BITRISE_API_TOKEN")},
+		"variables[BITRISE_APP_SLUG]":   {os.Getenv("BITRISE_APP_SLUG")},
+		"variables[BITRISE_BUILD_SLUG]": {os.Getenv("BITRISE_BUILD_SLUG")},
+	}
+
+	// Debug log for the form data
+	fmt.Printf("DEBUG: Sending pipeline trigger with data: %s\n", data.Encode())
 
 	req, err := http.NewRequest("POST", apiURL, strings.NewReader(data.Encode()))
 	if err != nil {
@@ -38,11 +54,14 @@ func triggerPipeline(cfg config) error {
 	}
 	defer resp.Body.Close()
 
+	// Debug log for response status
+	fmt.Printf("DEBUG: Response Status: %s\n", resp.Status)
+
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
 		return fmt.Errorf("server error: %s (code: %d)", resp.Status, resp.StatusCode)
 	}
 
-	log.Infof("Pipeline triggered successfully.")
+	fmt.Println("Pipeline triggered successfully.")
 	return nil
 }
 
